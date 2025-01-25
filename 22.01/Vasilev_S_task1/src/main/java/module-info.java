4c515733b9ec4e28a5f19fcf349f0be4651b4c7e
BZLOMJIK <semka12rus@gmail.com>
2025-01-25 09:44:16 +0300

    Delete 22.01 directory

--- 22.01/Vasilev_S_task1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.vasilev_semen.vasilev_s_task1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.vasilev_semen.vasilev_s_task1 to javafx.fxml;
-    exports ru.vasilev_semen.vasilev_s_task1;
-}
--- 22.01/Vasilev_S_task3/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.vasilev_semen.vasilev_s_task3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.vasilev_semen.vasilev_s_task3 to javafx.fxml;
-    exports ru.vasilev_semen.vasilev_s_task3;
-}
